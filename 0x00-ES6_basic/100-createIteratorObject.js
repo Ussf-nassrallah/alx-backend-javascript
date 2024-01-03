f140293756224b16c7c6c534761855793b5a37b7
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-03 12:08:09 +0100

    task 100 done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,10 @@
 export default function createIteratorObject(report) {
   const output = [];
   const keys = Object.keys(report.allEmployees);
+
   for (const key of keys) {
     output.push(...report.allEmployees[key]);
-  };
+  }
 
   return output;
 }
