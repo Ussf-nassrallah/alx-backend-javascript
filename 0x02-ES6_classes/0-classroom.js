3a28f144b47d833e052f63ed971877e381206419
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-04 10:39:29 +0100

    class: ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Implement a class named ClassRoom
+ * @maxStudentsSize: (Number) and assigned to _maxStudentsSize
+*/
+
+export default class ClassRoom {
+  /* ClassRoom */
+
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
