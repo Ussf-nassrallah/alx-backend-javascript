b8319a2cb5c73976ba7edb081c5a533980b8dfb6
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-09 11:01:18 +0100

    returns a Set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,12 @@
+/**
+ * setFromArray - function that returns a Set from an array.
+ * @arr: array
+ * return: Set
+ */
+
+const setFromArray = (arr) => {
+  const uniqueEle = new Set(arr);
+  return uniqueEle;
+};
+
+export default setFromArray;
