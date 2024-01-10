a68e9f3fb35457c0effe9b47ac627724a3f7ed65
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-10 15:32:15 +0100

    done: task-0 (render students table)

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,3 +20,27 @@ const student_2: Student = {
 }
 
 const studentsList: Student[] = [student_1, student_2];
+
+export const handleStudentsTable = (students: Student[]): void => {
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const tableBody = document.createElement('tbody');
+  const trHead = document.createElement('tr');
+
+  document.querySelector('body').appendChild(table);
+  table.appendChild(tableHead);
+  table.appendChild(tableBody);
+  tableHead.appendChild(trHead);
+
+  trHead.insertAdjacentHTML('beforeend', '<th>firstName</th>');
+  trHead.insertAdjacentHTML('beforeend', '<th>location</th>');
+
+  for (const student of students) {
+    const trBody = document.createElement('tr');
+    trBody.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    trBody.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    tableBody.appendChild(trBody);
+  }
+}
+
+handleStudentsTable(studentsList);
