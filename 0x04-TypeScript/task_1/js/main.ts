bbc122e0219240a8ee3d705e5f1ababb1f316ddf
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-10 16:24:03 +0100

    finish task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,12 +7,45 @@ interface Teacher {
   [index:string]: any;
 }
 
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
 
-console.log(teacher3);
+interface printTeacher {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher = (firstName: string, lastName: string): string => {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
+}
+
+function createStudent(ctor: IStudentClassConstructor, firstName: string, lastName: string): IStudentClass {
+  return new ctor(firstName, lastName);
+}
