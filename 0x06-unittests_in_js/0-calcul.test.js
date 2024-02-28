739cfaf151033074196e1749042f930089e8dcb3
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-28 16:59:06 +0100

    add: unittest for calcul.js (fix some issues)

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,20 +3,20 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('round of a', () => {
-    assert.equal(calculateNumber(18.90, 2), 21);
-    assert.equal(calculateNumber(1.9, 0), 2);
-    assert.equal(calculateNumber(3.50, 2), 6);
+    assert.equal(calculateNumber(15.78, 2), 18);
+    assert.equal(calculateNumber(1.2, 0), 1);
+    assert.equal(calculateNumber(3.5, 2), 6);
   });
 
   it('round of b', () => {
-    assert.equal(calculateNumber(2, 18.90), 21);
-    assert.equal(calculateNumber(0, 1.9), 2);
-    assert.equal(calculateNumber(2, 3.50), 6);
+    assert.equal(calculateNumber(2, 15.78), 18);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(2, 3.5), 6);
   });
 
   it('sum of a and b', () => {
-    assert.equal(calculateNumber(18.90, 20), 39);
-    assert.equal(calculateNumber(1.9, 0.2), 2);
-    assert.equal(calculateNumber(3.50, 2.9), 7);
+    assert.equal(calculateNumber(15.78, 2.5), 19);
+    assert.equal(calculateNumber(1.2, 0.2), 1);
+    assert.equal(calculateNumber(3.5, 2.7), 7);
   });
 });
