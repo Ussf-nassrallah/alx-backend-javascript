279286b1ee3499c640b39c3e7e21e1164899385e
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-28 16:18:59 +0100

    add: unittest for calcul.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,37 +3,22 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should equal 4 when (a is 2) and (b is 2)', () => {
-    const expectedOutput = 4;
-    const output = calculateNumber(2, 2);
-
-    assert.equal(output, expectedOutput);
+    assert.equal(calculateNumber(2, 2), 4);
   });
 
   it('should equal 4 when (a is 1) and (b is 3)', () => {
-    const expectedOutput = 4;
-    const output = calculateNumber(1, 3);
-
-    assert.equal(output, expectedOutput);
+    assert.equal(calculateNumber(1, 3), 4);
   });
 
   it('should equal 5 when (a is 1) and (b is 3.7)', () => {
-    const expectedOutput = 5;
-    const output = calculateNumber(1, 3.7);
-
-    assert.equal(output, expectedOutput);
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
 
   it('should equal 5 when (a is 1.2) and (b is 3.7)', () => {
-    const expectedOutput = 5;
-    const output = calculateNumber(1.2, 3.7);
-
-    assert.equal(output, expectedOutput);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
 
   it('should equal 6 when (a is 1.5) and (b is 3)', () => {
-    const expectedOutput = 6;
-    const output = calculateNumber(1.5, 3.7);
-
-    assert.equal(output, expectedOutput);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
