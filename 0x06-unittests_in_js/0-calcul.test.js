257c12df2c1478b55f45174969845b8ff483d1e4
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-28 16:26:42 +0100

    add: unittest for calcul.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,23 +2,38 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should equal 4 when (a is 2) and (b is 2)', () => {
-    assert.equal(calculateNumber(2, 2), 4);
+  it('floating point whole numbers', () => {
+    const expectedOutput = 4;
+    const output = calculateNumber(2.0, 2.0);
+
+    assert.equal(output, expectedOutput);
   });
 
-  it('should equal 4 when (a is 1) and (b is 3)', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+  it('floating point whole numbers', () => {
+    const expectedOutput = 4;
+    const output = calculateNumber(1.0, 3.0);
+
+    assert.equal(output, expectedOutput);
   });
 
   it('should equal 5 when (a is 1) and (b is 3.7)', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    const expectedOutput = 5;
+    const output = calculateNumber(1.0, 3.7);
+
+    assert.equal(output, expectedOutput);
   });
 
   it('should equal 5 when (a is 1.2) and (b is 3.7)', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    const expectedOutput = 5;
+    const output = calculateNumber(1.2, 3.7);
+
+    assert.equal(output, expectedOutput);
   });
 
-  it('should equal 6 when (a is 1.5) and (b is 3)', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+  it('should equal 6 when (a is 1.5) and (b is 3.7)', () => {
+    const expectedOutput = 6;
+    const output = calculateNumber(1.5, 3.7);
+
+    assert.equal(output, expectedOutput);
   });
 });
