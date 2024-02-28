b72a2270b70c2e17b253eecac02b9ba77a987444
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-28 17:33:27 +0100

    add: unittest for calcul_chai.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,20 @@
+/* Combining descriptions */
+
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    const sum = Math.round(a) + Math.round(b);
+    return sum;
+  }
+
+  if (type === 'SUBTRACT') {
+    const subtract = Math.round(a) - Math.round(b);
+    return subtract;
+  }
+
+  if (type === 'DIVIDE') {
+    const result = (b === 0) ? 'Error' : Math.round(a) / Math.round(b);
+    return result;
+  }
+};
+
+module.exports = calculateNumber;
