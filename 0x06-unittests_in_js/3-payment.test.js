903addd1922fe165da97a4aa2c57b1fc06a3c86b
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-29 11:59:10 +0100

    add: unittest for 3-payment.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,11 +1,16 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
-// const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   to('validate the usage of the Utils function', () => {
     const parent = sinon.spy(Utils);
-    expect(parent.calculateNumber.calledWith('SUM', 100, 20)).to.be.eq(true);
+
+    sendPaymentRequestToApi(100, 2);
+    expect(parent.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(parent.calculateNumber.callCount).to.be.eq(1);
+
+    parent.calculateNumber.restore();
   });
 });
